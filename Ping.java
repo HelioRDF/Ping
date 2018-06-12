fcdf181bacd4116523cfbc2fa2be13f7c346208f
Helio Franca <jpfranca@bol.com.br>
2018-06-12 10:57:27 -0300

    Teste

--- Ping.java
+++ Ping.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+//oi
 public class Ping {
 
 	public static void main(String[] args) throws IOException {
 
-		String saida = executarComando("ping 8.8.8.9");
+		String saida = "C:/Users/helio.franca/Desktop/Ping/Teste.bat";
+		executarComando(saida);
 
 	}
-
+//Teste xxx
 	public static String executarComando(String comando) throws IOException {
 		Process processo = Runtime.getRuntime().exec(comando);
 		BufferedReader entrada = new BufferedReader(new InputStreamReader(processo.getInputStream()));
